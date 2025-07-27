5ffe8d585ac2b0d0713eb2c4b1e2cf2b60c9482c
hamtorygoals <yhaemin0531@naver.com>
2025-07-27 14:46:51 +0900

    :tada:Start: 새 프로젝트 생성

---
+++ src/main/java/com/hackathon/junglegym/JunglegymApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.junglegym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunglegymApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JunglegymApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/hackathon/junglegym/JunglegymApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.junglegym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunglegymApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
