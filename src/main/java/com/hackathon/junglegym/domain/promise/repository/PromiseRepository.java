b95ac148a5c365fabb9d19ae8ffc4dad38952823
hamtorygoals <yhaemin0531@naver.com>
2025-08-13 14:46:33 +0900

    :sparkles: Feat: promise 완료도 조회 api add

--- src/main/java/com/hackathon/junglegym/domain/promise/repository/PromiseRepository.java
+++ src/main/java/com/hackathon/junglegym/domain/promise/repository/PromiseRepository.java
@@ -1,7 +1,12 @@
 package com.hackathon.junglegym.domain.promise.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.hackathon.junglegym.domain.promise.entity.Promise;
 
-public interface PromiseRepository extends JpaRepository<Promise, Long> {}
+public interface PromiseRepository extends JpaRepository<Promise, Long> {
+
+  List<Promise> findByCategory_Politician_Id(Long politicianId);
+}
