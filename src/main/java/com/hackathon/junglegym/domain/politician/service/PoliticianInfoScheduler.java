51b8ba7c3ee3bc2af8b3e44f0ce699b93366cfb8
E2YunJeong <dbsdbs3440@gmail.com>
2025-08-15 16:46:33 +0900

    :recycle:Refactor: 코드 스타일 변경

--- src/main/java/com/hackathon/junglegym/domain/politician/service/PoliticianInfoScheduler.java
+++ src/main/java/com/hackathon/junglegym/domain/politician/service/PoliticianInfoScheduler.java
@@ -1,9 +1,10 @@
 package com.hackathon.junglegym.domain.politician.service;
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Component
