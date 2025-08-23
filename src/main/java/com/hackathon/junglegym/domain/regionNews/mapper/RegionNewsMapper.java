27cf75ae1dc1a580ae958c987640e983c3a4de4a
hamtorygoals <yhaemin0531@naver.com>
2025-08-23 20:23:46 +0900

    :recycle: Refactor: media name refactor

--- src/main/java/com/hackathon/junglegym/domain/regionNews/mapper/RegionNewsMapper.java
+++ src/main/java/com/hackathon/junglegym/domain/regionNews/mapper/RegionNewsMapper.java
@@ -6,14 +6,22 @@ import com.hackathon.junglegym.domain.regionNews.entity.RegionNews;
 public class RegionNewsMapper {
 
   public static RegionNewsResponse toResponse(RegionNews e) {
+    String media =
+        (e.getMediaOrientation() != null)
+            ? e.getMediaOrientation().getMedia()
+            : e.getMediaName(); // 저장 시 제목에서 꼬리표 제거했으니 여기서 굳이 재추출할 필요 X
+
+    String mediaImgUrl =
+        (e.getMediaOrientation() != null) ? e.getMediaOrientation().getImgUrl() : null;
+
     return RegionNewsResponse.builder()
         .id(e.getId())
         .newsCategory(e.getCategory().getDescription())
-        .title(e.getTitle())
+        .title(e.getTitle()) // 이미 clean title
         .oneLineContent(e.getOneLineContent())
         .summary(e.getSummary())
-        .media(e.getMediaOrientation() != null ? e.getMediaOrientation().getMedia() : null)
-        .mediaImgUrl(e.getMediaOrientation() != null ? e.getMediaOrientation().getImgUrl() : null)
+        .media(media)
+        .mediaImgUrl(mediaImgUrl)
         .date(e.getDate())
         .link(e.getLink())
         .build();
