bd9b68a45150d9b039ee64b05692001e77e7911c
E2YunJeong <dbsdbs3440@gmail.com>
2025-08-25 22:56:33 +0900

    :recycle:Refactor: 정글챗 관련 법 조항 개행문자 추가

--- src/main/java/com/hackathon/junglegym/domain/qeustion/service/QdrantService.java
+++ src/main/java/com/hackathon/junglegym/domain/qeustion/service/QdrantService.java
@@ -201,7 +201,7 @@ public class QdrantService {
     String world = cut(snippet.trim(), 180);
     String sn = world.replace("\\R", "");
 
-    return base + "\n" + "「" + sn + "」";
+    return base + "\n\n" + "「" + sn + "」";
   }
 
   private static String cut(String s, int max) {
